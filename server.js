4475f54f578cbded30c229542bac1cf4b3f5811b
celidonnet <celdnnt@gmail.com>
2024-07-23 17:19:15 -0300

    My first commit

---
+++ server.js
@@ -0,0 +1,150 @@
+const express = require('express');
+const mysql = require('mysql2');
+const path = require('path');
+const app = express();
+
+app.use(express.json());
+app.use(express.static(path.join(__dirname, 'public')));
+
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'simulador_examenes'
+});
+
+connection.connect(err => {
+    if (err) {
+        console.error('Error connecting to the database:', err);
+        return;
+    }
+    console.log('Connected to the database');
+});
+
+app.get('/get-questions', (req, res) => {
+    const count = parseInt(req.query.count);
+    connection.query(`SELECT * FROM preguntas ORDER BY RAND() LIMIT ?`, [count], (err, results) => {
+        if (err) {
+            return res.status(500).send('Error retrieving questions');
+        }
+        res.json(results);
+    });
+});
+
+app.post('/corregir', (req, res) => {
+    const respuestasUsuario = req.body;
+
+    connection.query('SELECT * FROM respuestas_correctas', (err, correctAnswers) => {
+        if (err) {
+            return res.status(500).send('Error retrieving correct answers');
+        }
+
+        const respuestasCorrectasMap = correctAnswers.reduce((map, answer) => {
+            if (!map[answer.pregunta_id]) {
+                map[answer.pregunta_id] = [];
+            }
+            map[answer.pregunta_id].push(answer.opcion_correcta);
+            return map;
+        }, {});
+
+        const resultados = respuestasUsuario.map(({ preguntaId, userAnswers, index }) => {
+            const correctAnswer = respuestasCorrectasMap[preguntaId] || [];
+            const correcto = userAnswers.length === correctAnswer.length && userAnswers.every(val => correctAnswer.includes(val));
+            return { preguntaId, correcto, index };
+        });
+
+        res.json({ resultados });
+    });
+});
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+app.get('/questionPage', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'questionPage.html'));
+});
+
+app.listen(3000, () => {
+    console.log('Server is running on http://localhost:3000');
+});
+
+/* const express = require('express');
+const mysql = require('mysql2');
+const path = require('path');
+const app = express();
+
+app.use(express.json());
+app.use(express.static(path.join(__dirname, 'public')));
+
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'simulador_examenes'
+});
+
+connection.connect(err => {
+    if (err) {
+        console.error('Error connecting to the database:', err);
+        return;
+    }
+    console.log('Connected to the database');
+});
+
+app.get('/get-questions', (req, res) => {
+    const count = parseInt(req.query.count);
+    connection.query(`SELECT * FROM preguntas ORDER BY RAND() LIMIT ?`, [count], (err, results) => {
+        if (err) {
+            return res.status(500).send('Error retrieving questions');
+        }
+        res.json(results);
+    });
+});
+
+app.post('/corregir', (req, res) => {
+    const respuestasUsuario = req.body;
+
+    connection.query('SELECT * FROM respuestas_correctas', (err, correctAnswers) => {
+        if (err) {
+            return res.status(500).send('Error retrieving correct answers');
+        }
+
+        const respuestasCorrectasMap = correctAnswers.reduce((map, answer) => {
+            if (!map[answer.pregunta_id]) {
+                map[answer.pregunta_id] = [];
+            }
+            map[answer.pregunta_id].push(answer.opcion_correcta);
+            return map;
+        }, {});
+
+        const resultados = Object.keys(respuestasUsuario).map(preguntaId => {
+            const correctAnswer = respuestasCorrectasMap[preguntaId] || [];
+            const userAnswer = Array.isArray(respuestasUsuario[preguntaId]) ? respuestasUsuario[preguntaId] : [respuestasUsuario[preguntaId]];
+            const correcto = userAnswer.length === correctAnswer.length && userAnswer.every(val => correctAnswer.includes(val));
+            return { preguntaId, correcto };
+        });
+
+        // Ensure resultados are sorted by the original question order
+        resultados.sort((a, b) => {
+            const aIndex = parseInt(a.preguntaId);
+            const bIndex = parseInt(b.preguntaId);
+            return aIndex - bIndex;
+        });
+
+        res.json({ resultados });
+    });
+});
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+app.get('/questionPage', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'questionPage.html'));
+});
+
+app.listen(3000, () => {
+    console.log('Server is running on http://localhost:3000');
+}); */
+
